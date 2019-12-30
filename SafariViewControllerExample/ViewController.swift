0fea5fadd444a21dc7a5d9eeb5d67137a0979d6f
Isaac A. Murchie <isaac.appium@gmail.com>
2019-12-30 09:27:03 -0500

    Initial Commit

---
+++ SafariViewControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafariViewControllerExample
+//
+//  Created by Isaac Murchie on 12/30/19.
+//  Copyright © 2019 Isaac Murchie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
